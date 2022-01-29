19195993f8706d2c59dad4f0e366eef99dfb2531
Gusb <mihivanmih@gmail.com>
2022-01-29 22:55:48 +0300

    next JWT authorization; Login, Logout

--- auth_jwt_react_node/server/router/index.js
+++ auth_jwt_react_node/server/router/index.js
@@ -1,8 +1,12 @@
 import Router from 'express'
 import userController from '../controllers/user-controller.js'
 const router = new Router()
+import { body } from 'express-validator'
 
-router.post('/registration', userController.registrationUser)
+router.post('/registration',
+    body('email').isEmail(),
+    body('password').isLength({min:3, max:32}),
+    userController.registrationUser)
 router.post('/login', userController.login)
 router.post('/logout', userController.logout)
 router.get('/activate/:link', userController.activateUser)
